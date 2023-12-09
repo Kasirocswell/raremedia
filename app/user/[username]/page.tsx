a8aabb6459d61541077942b88ec1bb9c5b1dc401
Lewis Meyers <meyerslewis193@gmail.com>
2023-12-09 13:23:21 -0500

    mobile first design

--- app/user/[username]/page.tsx
+++ app/user/[username]/page.tsx
@@ -1,74 +1,83 @@
-'use client'
+"use client"
 
-import { useEffect, useState } from 'react';
-import { supabase } from '../../../supabase/client';
-import { getUser } from '../../../store/userData';
-import Navbar from '@/app/components/Navbar';
+import { useEffect, useState } from "react"
+import { supabase } from "../../../supabase/client"
+import { getUser } from "../../../store/userData"
+import Navbar from "@/app/components/Navbar"
 
 interface UserProfileData {
-    id: string;
-    username: string;
-    email: string;
-    profilePictureUrl: string; 
-    bannerUrl: string;        
-    bio: string;
+  id: string
+  username: string
+  email: string
+  profile_picture: string
+  banner: string
+  bio: string
 }
 
 const UserProfile = () => {
-  const { user } = getUser();
-  const [userProfile, setUserProfile] = useState<UserProfileData | null>(null);
+  const { user } = getUser()
+  const [userProfile, setUserProfile] = useState<UserProfileData | null>(null)
 
   const fetchImageUrls = async (path: string) => {
-    const { data } = supabase.storage.from('pfp').getPublicUrl(path);
-    return data?.publicUrl || ''; 
-  };
-  
+    const { data } = supabase.storage.from("pfp").getPublicUrl(path)
+    return data?.publicUrl || ""
+  }
 
   useEffect(() => {
     const fetchUserProfile = async () => {
       if (user && user.username) {
         const { data, error } = await supabase
-          .from('users')
-          .select('*')
-          .eq('username', user.username)
-          .single();
+          .from("users")
+          .select("*")
+          .eq("username", user.username)
+          .single()
 
         if (error) {
-          console.error('Error fetching user profile:', error);
+          console.error("Error fetching user profile:", error)
         } else if (data) {
-          const profilePictureUrl = await fetchImageUrls(data.profilePicture);
-          const bannerUrl = await fetchImageUrls(data.banner);
-          setUserProfile({ ...data, profilePictureUrl, bannerUrl });
+          console.log("USER DATA")
+          console.log(data)
+          const profilePictureUrl = await fetchImageUrls(data.profile_picture)
+          const bannerUrl = await fetchImageUrls(data.banner)
+          setUserProfile({ ...data })
         }
       }
-    };
+    }
 
-    fetchUserProfile();
-  }, [user]);
+    fetchUserProfile()
+  }, [user])
 
   if (!userProfile) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       <Navbar />
       <div>
-        <img src={userProfile.bannerUrl} alt="Banner" className="w-full" />
+        <img
+          src={userProfile.banner}
+          alt="Banner"
+          className="w-full rounded-xl"
+        />
       </div>
-      <div className='mx-auto'>
-        <img src={userProfile.profilePictureUrl} alt="Profile Picture" className="w-[100px] h-[100px] rounded-full" />
+      <div className="mx-auto mt-[100px] ">
+        <img
+          src={userProfile.profile_picture}
+          alt="Profile Picture"
+          className="w-[100px] h-[100px] rounded-full"
+        />
       </div>
-      <div className='flex-col mx-auto'>
-        <p>{userProfile.username}</p>
-        <p>{userProfile.bio}</p>
-        <p>Subscriptions</p>
-        <p>Joined</p>
+      <div className="flex-col mx-auto text-center">
+        <p className="justify-center mt-[20px] text-4xl">
+          {userProfile.username}
+        </p>
+        <p className=" mt-[100px] text-xl">{userProfile.bio}</p>
+        <p className="text-xl">Subscriptions</p>
+        <p className=" text-xl">Joined</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default UserProfile;
-
-
+export default UserProfile
